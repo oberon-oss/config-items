74c3ebf0b042b47db5ee170f0100e42715840dca
TigerLilly <info@oberon-oss.eu>
2026-08-08 16:55:45 +0200

    Remove redundant `public` modifiers in `StorableConfigurationItemBuilderAccessor` interface methods

--- src/main/java/eu/oberon/oss/tools/configitems/StorableConfigurationItemBuilderAccessor.java
+++ src/main/java/eu/oberon/oss/tools/configitems/StorableConfigurationItemBuilderAccessor.java
@@ -3,23 +3,23 @@ package eu.oberon.oss.tools.configitems;
 import java.util.function.Function;
 
 public interface StorableConfigurationItemBuilderAccessor<I, K, A, P> {
-    public Class<I> getIntKeyType();
+    Class<I> getIntKeyType();
 
-    public Class<K> getExtKeyType();
+    Class<K> getExtKeyType();
 
-    public Class<A> getApplicationDataType();
+    Class<A> getApplicationDataType();
 
-    public Class<P> getStorageType() ;
+    Class<P> getStorageType() ;
 
-    public Function<I, K> getToExtKey();
+    Function<I, K> getToExtKey();
 
-    public Function<K, I> getToIntKey();
+    Function<K, I> getToIntKey();
 
-    public Function<P, A> getToDataType();
+    Function<P, A> getToDataType();
 
-    public Function<A, P> getToStorageType();
+    Function<A, P> getToStorageType();
 
-    public I getItemID();
+    I getItemID();
 
-    public A getDefaultValue();
+    A getDefaultValue();
 }
